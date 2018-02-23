8c21cea43be0e8dd9bd5a238daa1c19e41ccddbb
Stephanie <esceeaitchyu@gmail.com>
2018-02-22 18:45:33 -0800

    Completion of 'Directives: Part 2' checkpoint

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
       var SongPlayer = {};
 
       /**
@@ -24,6 +24,12 @@
       SongPlayer.currentSong = null;
 
       /**
+      * @desc Current playback time (in seconds) of currently playing song
+      * @type {Number}
+      */
+      SongPlayer.currentTime = null;
+
+      /**
       * @desc Buzz object audio file
       * @type {Object}
       */
@@ -42,8 +48,16 @@
              formats: ['mp3'],
              preload: true
          });
+
+         currentBuzzObject.bind('timeupdate', function() {
+           $rootScope.$apply(function() {
+           SongPlayer.currentTime = currentBuzzObject.getTime();
+           });
+         });
+
          SongPlayer.currentSong = song;
       };
+
 
       /**
       *@function playSong
@@ -127,10 +141,21 @@
         }
     };
 
+    /**
+    * @function setCurrentTime
+    * @desc Set current time (in seconds) of currently playing song
+    * @param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+      if (currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+      }
+    };
+
          return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
